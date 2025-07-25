8384fd4cac346719699835a1befe53d5aca9bc7a
diiddiii <jjh.hahmshout@gmail.com>
2025-07-25 11:07:04 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function updateDateTime() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const date = String(now.getDate()).padStart(2, "0");
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+
+  document.getElementById("date").innerText = `${year}년 ${month}월 ${date}일`;
+  document.getElementById("time").innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateDateTime, 1000);
+updateDateTime();
