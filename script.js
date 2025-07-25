dd1a72d892b63a4ecca2ad3f48066094055ea532
diiddiii <jjh.hahmshout@gmail.com>
2025-07-25 11:13:34 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 function updateDateTime() {
   const now = new Date();
   const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, "0");
-  const date = String(now.getDate()).padStart(2, "0");
-  const hours = String(now.getHours()).padStart(2, "0");
-  const minutes = String(now.getMinutes()).padStart(2, "0");
-  const seconds = String(now.getSeconds()).padStart(2, "0");
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const date = String(now.getDate()).padStart(2, '0');
+  const hour = String(now.getHours()).padStart(2, '0');
+  const minute = String(now.getMinutes()).padStart(2, '0');
+  const second = String(now.getSeconds()).padStart(2, '0');
 
-  document.getElementById("date").innerText = `${year}년 ${month}월 ${date}일`;
-  document.getElementById("time").innerText = `${hours}:${minutes}:${seconds}`;
+  document.getElementById("date").textContent = `${year}년 ${month}월 ${date}일`;
+  document.getElementById("time").textContent = `${hour}:${minute}:${second}`;
 }
 
 setInterval(updateDateTime, 1000);
